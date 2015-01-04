2c417874f2b3959d53811bdb3b7ee9be7ec30ab5
lizhenning <254435828@qq.com>
2015-01-04 10:35:05 +0800

    init

---
+++ SwiftToday/SwiftToday/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftToday
+//
+//  Created by lizhenning on 15/1/4.
+//  Copyright (c) 2015年 lizhenning. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
